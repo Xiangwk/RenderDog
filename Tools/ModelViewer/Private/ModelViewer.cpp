46b3c14c6e7610587cfac33f0f6b9f93136635a0
Xiang Weikang <xiangwk1991@163.com>
2022-06-25 17:01:21 +0800

    修改示例贴图名称

--- Tools/ModelViewer/Private/ModelViewer.cpp
+++ Tools/ModelViewer/Private/ModelViewer.cpp
@@ -89,14 +89,14 @@ bool ModelViewer::Init(const ModelViewerInitDesc& desc)
 
 	/*m_pModel = new RenderDog::StaticModel();
 	m_pModel->LoadFromFile("Models/generator/generator_small.obj", "Shaders/StaticModelVertexShader.hlsl", "Shaders/PhongLightingPixelShader.hlsl");
-	m_pModel->LoadTextureFromFile(L"Textures/PolybumpTangent_DDN.tga");
+	m_pModel->LoadTextureFromFile(L"Textures/PolybumpTangent_norm.tga");
 	m_pModel->SetPosGesture(RenderDog::Vector3(0.0f, 0.0f, 0.0f), RenderDog::Vector3(90.0f, 0.0f, 0.0f), RenderDog::Vector3(0.1f));
 	m_pModel->RegisterToScene(m_pScene);*/
 	RenderDog::GeometryGenerator::StandardMeshData SphereMeshData;
 	RenderDog::g_pGeometryGenerator->GenerateSphere(40, 40, 5, SphereMeshData);
 	m_pModel = new RenderDog::StaticModel();
 	m_pModel->LoadFromStandardData(SphereMeshData.vertices, SphereMeshData.indices, "Shaders/StaticModelVertexShader.hlsl", "Shaders/PhongLightingPixelShader.hlsl", "Sphere");
-	m_pModel->LoadTextureFromFile(L"Textures/Brick_n.tga");
+	m_pModel->LoadTextureFromFile(L"Textures/Brick_norm.tga");
 	m_pModel->SetPosGesture(RenderDog::Vector3(0.0f, 0.0f, 0.0f), RenderDog::Vector3(0.0f, 0.0f, 0.0f), RenderDog::Vector3(1.0f));
 	m_pModel->RegisterToScene(m_pScene);
 
