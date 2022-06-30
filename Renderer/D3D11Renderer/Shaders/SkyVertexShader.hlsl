976cc156aaeb763d0687dd31a9a082355b482456
Xiang Weikang <xiangwk1991@163.com>
2022-06-30 23:21:48 +0800

    增加注释

--- Renderer/D3D11Renderer/Shaders/SkyVertexShader.hlsl
+++ Renderer/D3D11Renderer/Shaders/SkyVertexShader.hlsl
@@ -36,6 +36,7 @@ VSOutput Main(VSInput vsInput)
 	VSOutput vsOutput = (VSOutput)0;
 	float4 PosL = float4(vsInput.Pos, 1.0f);
 	vsOutput.Pos = mul(PosL, WorldMat);
+	//天空盒相对于相机静止
 	vsOutput.Pos.xyz += EyePosW.xyz;
 	vsOutput.Pos = mul(vsOutput.Pos, ViewMat);
 	vsOutput.Pos = mul(vsOutput.Pos, ProjMat);
